1ed38d404e2a7ad9e62dd8d7f0e2d9069f3a6a57
Raul Tarampi <jezreeldagan2001@mail.com>
2024-06-06 01:55:38 +0200

    not done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,20 +1,21 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getCountries } from "../services/Axios";
 import { ScaleLoader } from "react-spinners";
 
 const CountryDetails = () => {
     const { countryId } = useParams
     const [ loading, setLoading ] = useState(true)
-    const [ country, setCountries] = useState(true)
+    const [ country, setCountry] = useState(true)
 
     useEffect(() => {
         getCountries(countryId)
         .then((country) => {
-            setCountries(country)
+            setCountry(country)
         })
-        .catch(err => {console.error(err)
+        .catch(err => {
+            console.error(err)
         })
         .finally(() => {
             setLoading(false)
@@ -27,50 +28,43 @@ const CountryDetails = () => {
     return (
         <div className="container">
             <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+            <h1>{country.name?.common}</h1>
 
-            {loading ? <div className="container"><ScaleLoader /></div> : (
-                <>
-                    <h1>{country.name.official}</h1>
+            <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code?.toLowerCase()}.png`}
+                className="img-thumbnail"
+                alt={country.name?.common}
+                width={50}
+            />
 
-                    <img
-                        src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
-                        className="img-thumbnail"
-                        alt={country.name.common}
-                        width={50}
-                    ></img>
-
-                    <table className="table">
-                        <thead></thead>
-                        <tbody>
-                            <tr>
-                                <td style={{ width: "30%" }}>Capital</td>
-                                <td>{country.capital}</td>
-                            </tr>
-                            <tr>
-                                <td>Area</td>
-                                <td>
-                                    {country.area} km
-                                    <sup>2</sup>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Borders</td>
-                                <td>
-                                    <ul style={{ listStyle: "none" }}>
-                                        {country.borders.map((border, i) => (
-                                            <li key={i}>
-                                                <Link to={`/${border}`}>{border}</Link>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </>
-            )}
+            <table className="table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <td style={{ width: "30%" }}>Capital</td>
+                        <td>{country.capital}</td>
+                    </tr>
+                    <tr>
+                        <td>Area</td>
+                        <td>{country.area} km<sup>2</sup></td>
+                    </tr>
+                    <tr>
+                        <td>Borders</td>
+                        <td>
+                            <ul style={{ listStyle: "none" }}>
+                                {country.borders?.map((border, i) => (
+                                    <li key={i}>
+                                        <Link to={`/${border}`}>{border}</Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-    )
+    );
+
 }
 
 export default CountryDetails;
